a85a6bca13eb8a3127b15147c3f1990ae39cb93b
JKL90 <167874464+JKL90@users.noreply.github.com>
2025-05-07 23:51:38 +0200

    Revoir le bouton genereréé

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -171,6 +171,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="../../dist/js/adminlte.min.js"></script>
     <!-- Toastr -->
     <script src="../../plugins/toastr/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- preloader -->
     <script>
